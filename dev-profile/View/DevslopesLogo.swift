dc2763952cc65a389b07008f5dce4f9105d9481e
User <user@users-macbook.home>
2018-10-17 16:22:02 +0100

    built layout for IPhone

---
+++ dev-profile/View/DevslopesLogo.swift
@@ -0,0 +1,22 @@
+//
+//  DevslopesLogo.swift
+//  dev-profile
+//
+//  Created by User on 15/10/2018.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class DevslopesLogo: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.masksToBounds = true
+        layer.cornerRadius = 15
+}
+}
+
+
+
+
